c8a7c0f7f64109adece240e86a47e6daecb6c7aa
Ed dos Prazeres <edimilsonpereira.pro@gmail.com>
2019-09-14 22:21:24 -0300

    Add Sass on project

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
-import { Container } from './styles'
-
+import './Home.scss'
 class Home extends Component {
   render() {
     return (
-      <Container>
-        <img
-          alt="vindicce-logo.png" 
-          src="https://user-images.githubusercontent.com/22872282/59152673-45bacc80-8a1f-11e9-9ca4-9050c446a17a.png"/>
-      </Container>
+      <div className="Home">
+        Home
+      </div>
     )
   }
 }
